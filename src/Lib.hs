09c5212fb50997a41ed6a4b489ea56e091824b53
Mike Schaeffer <mike@mschaef.com>
2023-12-11 17:46:44 -0500

    Solution to day 1, part 1.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
 module Lib
-    ( someFunc
+    ( day1
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Char (isDigit, ord)
+
+digits :: String -> String
+digits s = filter isDigit s
+
+digitVal :: Char -> Int
+digitVal ch = (ord ch) - (ord '0')
+
+lineValue :: String -> Int
+lineValue line = do
+  let lineDigits = digits line
+  digitVal (head lineDigits) * 10 + digitVal (last lineDigits)
+
+fileLines :: String -> IO [ String ]
+fileLines fileName = lines <$> readFile fileName
+
+day1 :: IO ()
+day1 = do
+  fl <- fileLines "input-day-1.txt"
+  let total = sum (map lineValue fl)
+
+  putStrLn $ "Sum = " <> show total
+
+  putStrLn "end run."
