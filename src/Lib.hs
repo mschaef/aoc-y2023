72491693abd119ce6c9564fdb88961a4049b6953
Mike Schaeffer <mike@mschaef.com>
2023-12-12 12:53:19 -0500

    Extend the digit list to include spelled out digits. This satisfies the second part of day 1.

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,16 +7,16 @@ import Data.Maybe (catMaybes)
 
 digitStrings :: [(String, Int)]
 digitStrings = [
-  ("0", 0),
-  ("1", 1),
-  ("2", 2),
-  ("3", 3),
-  ("4", 4),
-  ("5", 5),
-  ("6", 6),
-  ("7", 7),
-  ("8", 8),
-  ("9", 9)]
+  ("0", 0), ("zero" , 0),
+  ("1", 1), ("one"  , 1),
+  ("2", 2), ("two"  , 2),
+  ("3", 3), ("three", 3),
+  ("4", 4), ("four" , 4),
+  ("5", 5), ("five" , 5),
+  ("6", 6), ("six"  , 6),
+  ("7", 7), ("seven", 7),
+  ("8", 8), ("eight", 8),
+  ("9", 9), ("nine" , 9)]
 
 digitValAt :: String -> Maybe Int
 digitValAt atStr = fmap snd $ find (\( s, _ ) -> isPrefixOf s atStr) digitStrings
