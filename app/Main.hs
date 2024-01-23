96903bbbfe297fa75bbc713c3fbc3c306c295df1
Mike Schaeffer <mike@mschaef.com>
2024-01-23 10:19:30 -0500

    Add expected values to the runs.

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,19 @@ module Main (main) where
 import Day1
 import Day2
 
-runTest :: String -> IO Int -> IO ()
-runTest name fn = do
+runTest :: String -> IO Int -> Int -> IO ()
+runTest name fn expected = do
   result <- fn
-  putStrLn $ name <> " result = " <> show result
+  putStrLn $ name <> " result = " <> show result <> " " <> show expected
 
 main :: IO ()
 main = do
 
-  runTest "Day 1A" day1A
-  runTest "Day 1B" day1B
+  runTest "Day 1A" day1A 55208
+  runTest "Day 1B" day1B 54578
 
-  runTest "Day 2A" day2A
-  runTest "Day 2B" day2B
+  runTest "Day 2A" day2A 2406
+  runTest "Day 2B" day2B 78375
 
   putStrLn "end run."
 
